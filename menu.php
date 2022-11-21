26d2898b2fd270119afb8d05e3b558cdf1f99443
GreatSausage <cliffordvillenaaa@gmail.com>
2022-11-21 18:51:50 +0800

    gonna clean this later

--- menu.php
+++ menu.php
@@ -12,5 +12,86 @@
     <!--header-->
     <?php include "header.php"; ?>
 
+    <!--main-->
+    <div class="main">
+        <div class="section-title">
+        <h2>OUR MENU</h2>
+        </div>
+        <div class="menus">
+            <div class="menu-column">
+                <h4>breakfast</h4>
+                <div class="single-menu">
+                    <img src="https://via.placeholder.com/150C" alt="">
+                    <div class="menu-content">
+                        <h5>food title <span>$50</span></h5>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+                <div class="single-menu">
+                    <img src="https://via.placeholder.com/150C" alt="">
+                    <div class="menu-content">
+                        <h5>food title <span>$50</span></h5>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+                <div class="single-menu">
+                    <img src="https://via.placeholder.com/150C" alt="">
+                    <div class="menu-content">
+                        <h5>food title <span>$50</span></h5>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="menu-column">
+                <h4>lunch</h4>
+                <div class="single-menu">
+                    <img src="https://via.placeholder.com/150C" alt="">
+                    <div class="menu-content">
+                        <h5>food title <span>$50</span></h5>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+                <div class="single-menu">
+                    <img src="https://via.placeholder.com/150C" alt="">
+                    <div class="menu-content">
+                        <h5>food title <span>$50</span></h5>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+                <div class="single-menu">
+                    <img src="https://via.placeholder.com/150C" alt="">
+                    <div class="menu-content">
+                        <h5>food title <span>$50</span></h5>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="menu-column">
+                <h4>dinner</h4>
+                <div class="single-menu">
+                    <img src="https://via.placeholder.com/150C" alt="">
+                    <div class="menu-content">
+                        <h5>food title <span>$50</span></h5>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+                <div class="single-menu">
+                    <img src="https://via.placeholder.com/150C" alt="">
+                    <div class="menu-content">
+                        <h5>food title <span>$50</span></h5>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+                <div class="single-menu">
+                    <img src="https://via.placeholder.com/150C" alt="">
+                    <div class="menu-content">
+                        <h5>food title <span>$50</span></h5>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
