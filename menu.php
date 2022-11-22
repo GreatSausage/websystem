287b7b70b5083c756868263b967fc65b902b82fc
GreatSausage <cliffordvillenaaa@gmail.com>
2022-11-22 21:46:41 +0800

    gonna start doing this page tomorrow

--- menu.php
+++ menu.php
@@ -4,94 +4,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/fontawesome.min.css" integrity="sha512-RvQxwf+3zJuNwl4e0sZjQeX7kUa3o82bDETpgVCH2RiwYSZVDdFJ7N/woNigN/ldyOOoKw8584jM4plQdt8bhA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="menu.css?v=<?php echo time(); ?>">
-    <title>Menu - Garcia's Panciteria</title>
 </head>
 <body>
-    
-    <!--header-->
-    <?php include "header.php"; ?>
+    <header>
+        <!--Navbar-->
+            <div class="container-fluid">
+                <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand" href="index.php">
+                    <img src="../Ester_Pansitan/logo.png" alt="">&nbsp&nbspGarcia's Panciteria</a>
 
-    <!--main-->
-    <div class="main">
-        <div class="section-title">
-        <h2>OUR MENU</h2>
-        </div>
-        <div class="menus">
-            <div class="menu-column">
-                <h4>breakfast</h4>
-                <div class="single-menu">
-                    <img src="https://via.placeholder.com/150C" alt="">
-                    <div class="menu-content">
-                        <h5>food title <span>$50</span></h5>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                </div>
-                <div class="single-menu">
-                    <img src="https://via.placeholder.com/150C" alt="">
-                    <div class="menu-content">
-                        <h5>food title <span>$50</span></h5>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                </div>
-                <div class="single-menu">
-                    <img src="https://via.placeholder.com/150C" alt="">
-                    <div class="menu-content">
-                        <h5>food title <span>$50</span></h5>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="menu-column">
-                <h4>lunch</h4>
-                <div class="single-menu">
-                    <img src="https://via.placeholder.com/150C" alt="">
-                    <div class="menu-content">
-                        <h5>food title <span>$50</span></h5>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                </div>
-                <div class="single-menu">
-                    <img src="https://via.placeholder.com/150C" alt="">
-                    <div class="menu-content">
-                        <h5>food title <span>$50</span></h5>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                </div>
-                <div class="single-menu">
-                    <img src="https://via.placeholder.com/150C" alt="">
-                    <div class="menu-content">
-                        <h5>food title <span>$50</span></h5>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="menu-column">
-                <h4>dinner</h4>
-                <div class="single-menu">
-                    <img src="https://via.placeholder.com/150C" alt="">
-                    <div class="menu-content">
-                        <h5>food title <span>$50</span></h5>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                </div>
-                <div class="single-menu">
-                    <img src="https://via.placeholder.com/150C" alt="">
-                    <div class="menu-content">
-                        <h5>food title <span>$50</span></h5>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                </div>
-                <div class="single-menu">
-                    <img src="https://via.placeholder.com/150C" alt="">
-                    <div class="menu-content">
-                        <h5>food title <span>$50</span></h5>
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+        <!--hamburger-->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+                    
+        <!--menulist-->
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="menu.php" class="nav-link">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                    </li>
+                </ul>
+            </div>       
+    </header>
+
+    <main>
+        <h3> Admin Page </h3>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
 </body>
 </html>
