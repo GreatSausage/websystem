e61089b4c4233af88ba525858edb54e7d19cf785
GreatSausage <cliffordvillenaaa@gmail.com>
2022-11-24 12:05:23 +0800

    login page's done

--- login.php
+++ login.php
@@ -12,15 +12,13 @@
                 header("Location: index.php ");
             }
             else{
-                echo
-                "<script> alert('Incorrect Username/Email or Password'); </script>";
+                echo "<script> alert('Incorrect Username/Email or Password'); </script>";
             }
         }
             else{
-                echo
-                "<script> alert('User Not Registered'); </script>";
+                echo "<script> alert('User Not Registered'); </script>";
             }
-        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -34,32 +32,32 @@
     <title>Log In - Garcia's Panciteria </title>
 </head>
 <body>
-<header>
-        <!--Navbar-->
-            <div class="container-fluid">
-                <nav class="navbar navbar-expand-lg">
-                <a class="navbar-brand" href="index.php">
-                    <img src="../Ester_Pansitan/logo.png" alt="">&nbsp&nbspGarcia's Panciteria</a>
+    <header>
+    <!--Navbar-->
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg">
+            <a class="navbar-brand" href="index.php">
+                <img src="../Ester_Pansitan/logo.png" alt="">&nbsp&nbspGarcia's Panciteria</a>
 
-        <!--hamburger-->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <!--hamburger-->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
                     
-        <!--menulist-->
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="menu.php" class="nav-link">Menu</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="about.php">About</a>
-                    </li>
-                </ul>
-            </div>       
+    <!--menulist-->
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="menu.php" class="nav-link">Menu</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">About</a>
+                </li>
+            </ul>
+        </div>       
     </header>
 
     <main>
@@ -80,7 +78,7 @@
     </div>
     </main>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
 </body>
 </html>
