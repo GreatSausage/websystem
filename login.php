17b412c239bdef4907e6632cf9c48e71353497bd
GreatSausage <cliffordvillenaaa@gmail.com>
2022-11-27 10:21:03 +0800

    done

--- login.php
+++ login.php
@@ -34,31 +34,33 @@
 <body>
     <header>
         <!--Navbar-->
-            <div class="container-fluid">
-                <nav class="navbar navbar-expand-lg">
-                <a class="navbar-brand" href="index.php">&nbsp&nbspWelcome to Garcia's Panciteria</a>
-
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand" href="index.php">
+                <img src="../Ester_Pansitan/logo.png" alt="">&nbsp&nbspGarcia's Panciteria</a>
         <!--hamburger-->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </nav>
+        </div>
     </header>
+
     <main>
-    <!--form-->
-    <div class="box"> 
-        <form class="" action="" method="post" autocomplete="off"> 
-            <h2>Log In Form</h2> 
-            <div class="inputBox"> 
-                <input type="text" name="username" id="username" required value=""> <span>Username</span> <i></i> 
-            </div> 
-            <div class="inputBox"> 
-                <input type="password" name="password" id="password" required value=""> <span>Password</span> <i></i> 
-            </div> 
-            <div class="links"> <a href="register.php">Don't have an account? Sign Up Now</a> 
-            </div> 
-            <button type="submit" name="submit" class="submit-btn">Sign In</button>
-        </form> 
-    </div>
+        <!--form-->
+        <div class="box"> 
+            <form class="" action="" method="post" autocomplete="off"> 
+                <h2>Log In Form</h2> 
+                <div class="inputBox"> 
+                    <input type="text" name="username" id="username" required value=""> <span>Username</span> <i></i> 
+                </div> 
+                <div class="inputBox"> 
+                    <input type="password" name="password" id="password" required value=""> <span>Password</span> <i></i> 
+                </div> 
+                <div class="links"> <a href="register.php">Don't have an account? Sign Up Now</a></div>  
+                <button type="submit" name="submit" class="submit-btn">Sign In</button>
+            </form> 
+        </div>
     </main>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
