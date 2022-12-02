e5df1c18fe83dfc324a71768b138e489dec4cb33
GreatSausage <cliffordvillenaaa@gmail.com>
2022-12-02 17:40:02 +0800

    not done

--- menu.php
+++ menu.php
@@ -1,3 +1,64 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+};
+
+if(isset($_POST['add_to_wishlist'])){
+
+    $product_id = $_POST['product_id'];
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+
+    $check_wishlist_numbers = mysqli_query($conn, "SELECT * FROM `wishlist` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+    if(mysqli_num_rows($check_wishlist_numbers) > 0){
+        $message[] = 'already added to wishlist';
+    }elseif(mysqli_num_rows($check_cart_numbers) > 0){
+        $message[] = 'already added to cart';
+    }else{
+        mysqli_query($conn, "INSERT INTO `wishlist`(user_id, pid, name, price, image) VALUES('$user_id', '$product_id', '$product_name', '$product_price', '$product_image')") or die('query failed');
+        $message[] = 'product added to wishlist';
+    }
+
+}
+
+if(isset($_POST['add_to_cart'])){
+
+    $product_id = $_POST['product_id'];
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+    $product_quantity = $_POST['product_quantity'];
+
+    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+    if(mysqli_num_rows($check_cart_numbers) > 0){
+        $message[] = 'already added to cart';
+    }else{
+
+        $check_wishlist_numbers = mysqli_query($conn, "SELECT * FROM `wishlist` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+        if(mysqli_num_rows($check_wishlist_numbers) > 0){
+            mysqli_query($conn, "DELETE FROM `wishlist` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+        }
+
+        mysqli_query($conn, "INSERT INTO `cart`(user_id, pid, name, price, quantity, image) VALUES('$user_id', '$product_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
+        $message[] = 'product added to cart';
+    }
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,13 +70,12 @@
     <link rel="stylesheet" href="menu.css?v=<?php echo time(); ?>">
 </head>
 <body>  
-    <!--header-->
-    <header>
+<header>
         <!--Navbar-->
             <div class="container-fluid">
                 <nav class="navbar navbar-expand-lg">
-                <a class="navbar-brand" href="index.php">
-                    <img src="../Ester_Pansitan/logo.png" alt="">&nbsp&nbspGarcia's Panciteria</a>
+                <a class="navbar-brand" href="home.php">
+                    <img src="../Admin/assets/imgs/logo.png" alt="">&nbsp&nbspGarcia's Panciteria</a>
 
         <!--hamburger-->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,7 +86,7 @@
             <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                        <a class="nav-link" aria-current="page" href="home.php">Home</a>
                     </li>
                     <li class="nav-item">
                         <a href="menu.php" class="nav-link">Menu</a>
@@ -34,117 +94,62 @@
                     <li class="nav-item">
                         <a class="nav-link" href="about.php">About</a>
                     </li>
-                    <!--
                     <li class="nav-item">
-                        <a class="nav-link" href="logout.php">Logout</a>
+                        <a class="nav-link" href="cart.php" alt="">
+                            <span class="icon">
+                            <ion-icon name="cart-outline"></ion-icon>
+                        </span></a>
                     </li>
-                    -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php" alt="">
+                            <span class="icon">
+                            <ion-icon name="log-out-outline"></ion-icon>
+                        </span></a>
+                    </li>
                 </ul>
             </div>       
     </header>
-
-    <!--main-->
-    <main>
+    
+<!--main-->
+<main>
         <div class="section-title">
             <h1>Garcia's Panciteria</h1>
-            <h3>&mdash; Menu Page &mdash;</h3>
+            <h3>&mdash; MENU PAGE &mdash; </h3>
         </div><!--section-title-->
-        <div class="menu">
-            <div class="menu-column"><!--FIRST COLUMN STARTS-->
-                <h4>Small size</h4>
-                <div class="single-menu"><!--SMALL BIHON-->
-                    <img src="smallbihon.jpg" alt="">
-                    <div class="content-menu">
-                        <h5>Bihon</h5>
-                        <h6><span>P300 / P400</span></h6>
-                        <p>5 to 6 person</p>
-                        <a href="checkout.php" class="button">Order Now!</a>
-                    </div><!--content-menu-->
-                </div><!--single-menu-->
-                <div class="single-menu"><!--SMALL CANTON-->
-                    <img src="smallcanton.jpg" alt="">
-                    <div class="content-menu">
-                        <h5>Canton</h5>
-                        <h6><span>P300 / P400</span></h6>
-                        <p>5 to 6 person</p>
-                        <a href="checkout.php" class="button">Order Now!</a>
-                    </div><!--CONTENT-MENU-->
-                </div><!--SINGLE-MENU-->
-                <div class="single-menu"><!--SMALL MIKI-->
-                    <img src="smallmiki.jpg" alt=""><!--NO PICTURE FOR SMALL MIKI YET-->
-                    <div class="content-menu">
-                        <h5>Miki</h5>
-                        <h6><span>P300 / P400</span></h6>
-                        <p>5 to 6 person</p>
-                        <a href="checkout.php" class="button">Order Now!</a>
-                    </div><!--CONTENT-MENU-->
-                </div><!--SINGLE-MENU-->
-                </div><!--FIRST COLUMN ENDS-->
 
-            <div class="menu-column"><!--SECOND COLUMN STARTS-->
-                <h4>medium size</h4>
-                <div class="single-menu"><!--MEDIUM BIHON-->
-                    <img src="mediumbihon.jpg" alt="">
-                    <div class="content-menu">
-                        <h5>Bihon</h5>
-                        <h6><span>P500 / P600</span></h6>
-                        <p>10 to 12 person</p>
-                        <a href="checkout.php" class="button">Order Now!</a>
-                    </div><!--CONTENT-MENU-->
-                </div><!--SINGLE-MENU-->
-                <div class="single-menu"><!--MEDIUM CANTON-->
-                    <img src="mediumcanton.jpg" alt="">
-                    <div class="content-menu">
-                        <h5>Canton</h5>
-                        <h6><span>P500 / P600</span></h6>
-                        <p>10 to 12 person</p>>
-                        <a href="checkout.php" class="button">Order Now!</a>
-                    </div><!--CONTENT-MENU-->
-                </div><!--SINGLE-MENU-->
-                <div class="single-menu"><!--MEDIUM MIKI-->
-                    <img src="mediummiki.jpg" alt=""><!--NO PICTURE FOR MEDIUM MIKI YET-->
-                    <div class="content-menu">
-                        <h5>miki</h5>
-                        <h6><span>P500 / P600</span></h6>
-                        <p>10 to 12 person</p>
-                        <a href="checkout.php" class="button">Order Now!</a>
-                    </div><!--CONTENT-MENU-->
-                </div><!--SINGLE-MENU-->
-            </div><!--SECOND MENU COLUMN-->
-
-            <div class="menu-column">
-                <h4>large size</h4>
-                <div class="single-menu"><!--LARGE BIHON-->
-                    <img src="largebihon.jpg" alt="">
-                    <div class="content-menu">
-                        <h5>Bihon</h5>
-                        <h6><span>P700 / P800</span></h6>
-                        <p>13 to 15 person</p>
-                        <a href="checkout.php" class="button">Order Now!</a>
-                    </div><!--CONTENT-MENU-->
-                </div><!--SINGLE-MENU-->
-                <div class="single-menu"><!--LARGE CANTON-->
-                    <img src="largecanton.jpg" alt="">
-                    <div class="content-menu">
-                        <h5>Canton</h5>
-                        <h6><span>P700 / P800</span></h6>
-                        <p>13 to 15 person</p>
-                        <a href="checkout.php" class="button">Order Now!</a>
-                    </div><!--CONTENT-MENU-->
-                </div><!--SINGLE-MENU-->
-                <div class="single-menu"><!--LARGE MIKI-->
-                    <img src="largemiki.jpg" alt=""><!--NO PICTURE FOR LARGE MIKI YET-->
-                    <div class="content-menu">
-                        <h5>miki</h5>
-                        <h6><span>P700 / P800</span></h6>
-                        <p>13 to 15 person</p>
-                        <a href="checkout.php" class="button">Order Now!</a>
-                    </div><!--CONTENT-MENU-->
-                </div><!--SINGLE-MENU-->
-            </div><!--THIRD MENU COLUMN-->
-        </div>
+    <section class="single-menu">
+        <div class="menu-content">
+    <?php
+         $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
+         if(mysqli_num_rows($select_products) > 0){
+            while($fetch_products = mysqli_fetch_assoc($select_products)){
+      ?>
+    <form action="" method="POST" class="box">
+    <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="" class="image">
+         <h6><div class="name"><?php echo $fetch_products['name']; ?></div></h6>
+         <h6><div class="price">₱<?php echo $fetch_products['price']; ?>.00</div></h6>
+         <h6><div class="details"><?php echo $fetch_products['details']; ?></div></h6>
+         <input type="number" name="product_quantity" value="1" min="0" class="qty">
+         <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
+         <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+         <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+         <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+         <input type="submit" value="add to cart" name="add_to_cart" class="button">
+    
+    </form>
+    
+    <?php
+    }
+ }else{
+    echo '<p class="empty">no products added yet!</p>';
+ }
+ ?>
+                </section>
     </main>
-
+    
+    
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
