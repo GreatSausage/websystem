c6e9a7eaed2cbde29ca4a5604182a0f7e28f7ea4
GreatSausage <cliffordvillenaaa@gmail.com>
2022-12-02 17:56:59 +0800

    adds new file

---
+++ admin_header.php
@@ -0,0 +1,84 @@
+<!-- =============== Navigation ================ -->
+<div class="container">
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="admin_page.php">
+                        <span class="logo">
+                            <img src="../Admin/assets/imgs/logo.png" alt="">
+                        </span>
+                        <span class="b-name">Garcia's Panceteria</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="admin_page.php">
+                        <span class="icon">
+                            <ion-icon name="home-outline"></ion-icon>
+                        </span>
+                        <span class="title">Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="admin_users.php">
+                        <span class="icon">
+                            <ion-icon name="people-outline"></ion-icon>
+                        </span>
+                        <span class="title">Users</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="admin_products.php">
+                        <span class="icon">
+                            <ion-icon name="cube-outline"></ion-icon>
+                        </span>
+                        <span class="title">Products</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="admin_orders.php">
+                        <span class="icon">
+                            <ion-icon name="cart-outline"></ion-icon>
+                        </span>
+                        <span class="title">Orders</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="logout.php">
+                        <span class="icon">
+                            <ion-icon name="log-out-outline"></ion-icon>
+                        </span>
+                        <span class="title">Sign Out</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <!-- ========================= Main ==================== -->
+        <div class="main">
+            <div class="topbar">
+                <div class="toggle">
+                    <ion-icon name="menu-outline"></ion-icon>
+                </div>
+
+                <div class="search">
+                    <label>
+                        <input type="text" placeholder="Search here">
+                        <ion-icon name="search-outline"></ion-icon>
+                    </label>
+                </div>
+
+                <div class="user">
+                    <!--=====Don't Insert Anything here!=====-->
+                </div>
+            </div>
+
+            <!-- =========== Scripts =========  -->
+    <script src="assets/js/main.js"></script>
+
+<!-- ====== ionicons ======= -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
